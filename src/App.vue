dafb44a2fbeb79b4ab3c1523cacaecc60c02dca6
josephgeorge.git <josephgeorge.git@gmail.com>
2021-07-29 21:03:08 +1000

    Vpply-Code-Test

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <div v-if="showInstructions">
+      <Instructions />
+    </div>
+
+      <button @click="showInstructions=!showInstructions"><span v-if="!showInstructions">Show</span><span v-if="showInstructions">Hide</span> instructions</button>
+
+      <!-- Start writing your html here. Make sure to use at least 1 component -->
+    <div>
+      <div>
+        <!-- use this component to render each unique card. Hint:you should use v-for and v-bind here-->
+        <ListItem />
+      </div>
+      <div>
+        <!-- add the code to create a new business card here. Hint: you should use v-model here-->
+      </div>
+    </div> 
+  
+  </div>
+</template>
+
+<script>
+import Instructions from '@/components/Instructions.vue'
+import ListItem from '@/components/ListItem.vue'
+export default {
+  components: {Instructions, ListItem},
+  data(){
+    return{
+      showInstructions: true,
+      // maintain this data structure for your solution
+      items: [
+        {first_name: '', last_name: '', job_title: '', experience: 0, email: '', phone: 0},
+      ]
+    }
+  },
+  methods: {
+    newCard(){
+        return
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+html{
+  background-color: #fafafa
+}
+</style>
---
+++ src/components/Instructions.vue
@@ -0,0 +1,29 @@
+<template>
+  <p>Welcome to the Vpply Coding Test! </p>
+      
+      <p>In this coding test, you will build a basic application that users can create business cards using input fields. Those cards will render in a list.
+        <br><br>
+        Watch the sample video in the root folder to see an example of what your solution should do
+        <br><br>
+        Your solution does not need to look identical to the example, this test is just to show that you understand the basics of vuejs
+        <br><br>
+        There are some requirments you will need to follow, those include
+      </p>
+        <ul>
+          <li>use v-for to render the list of business cards</li>
+          <li>conditionally render the create new card functionality using v-if</li>
+          <li>v-model for data binding on the create new card feature</li>
+          <li>Use the ListItem component that has been defined already</li>
+          <li>Use v-bind to pass props to the ListItem component</li>
+        </ul>
+
+        <p>Once you complete the test, push your solution to a git repository and share it via email to joseph@vpply.com</p>
+        <p>Good Luck!</p>
+</template>
+
+<style scoped>
+*{
+    font-size: 18px;
+}
+
+</style>
