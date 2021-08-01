9ca4b95b7f8134816fa88f0f96c75d0a201b717b
josephgeorge <josephgeorge.git@gmail.com>
2021-08-01 23:51:23 +1000

    Update to Test

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,23 @@
 <template>
-  <div>
-    <div v-if="showInstructions">
-      <Instructions />
-    </div>
-
-      <button @click="showInstructions=!showInstructions"><span v-if="!showInstructions">Show</span><span v-if="showInstructions">Hide</span> instructions</button>
-
-      <!-- Start writing your html here. Make sure to use at least 1 component -->
+  <div>    
     <div>
-      <div>
-        <!-- use this component to render each unique card. Hint:you should use v-for and v-bind here-->
-        <ListItem />
-      </div>
-      <div>
-        <!-- add the code to create a new business card here. Hint: you should use v-model here-->
-      </div>
-    </div> 
-  
+      <!-- use this component to render each unique card. Hint:you should use v-for and v-bind here-->
+      <BusinessCard />
+    </div>
+    <div>
+      <!-- add the code to create a new business card here.-->
+    </div>
   </div>
 </template>
 
 <script>
-import Instructions from '@/components/Instructions.vue'
-import ListItem from '@/components/ListItem.vue'
+import BusinessCard from '@/components/BusinessCard.vue'
 export default {
-  components: {Instructions, ListItem},
+  components: {
+    BusinessCard
+  }, 
   data(){
     return{
-      showInstructions: true,
       // maintain this data structure for your solution
       items: [
         {first_name: '', last_name: '', job_title: '', experience: 0, email: '', phone: 0},
---
+++ src/components/BusinessCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <!-- This is the BusinessCard component -->
+    <!-- This component should display:
+            - First and last name
+            - Years experience and job title
+            - mobile number
+            - email address -->
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+
+</style>
